cdff0752479d096f92d1f6707647910b67ab8e3b
Eduardo Lima <99769751+PortifoliodoEdu@users.noreply.github.com>
2024-04-24 20:34:34 -0300

    Delete Tarefa 2 directory

--- Tarefa 2/OrdemCrescente.java
+++
@@ -1,1 +0,0 @@
-// Ler dois valores (considere que não serão lidos valores iguais) e escrevê-los em ordem crescente.
--- Tarefa 2/PositivoNegativo.java
+++
@@ -1,1 +0,0 @@
-// Ler um valor e escrever se é positivo ou negativo (considere o valor zero como positivo). 
